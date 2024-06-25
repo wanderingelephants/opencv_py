c1b43a37568af5afab90f5d74280ff6c7449e9a1
sandyptc@gmail.com <sandyptc@gmail.com>
2024-06-25 11:24:17 +0530

    Made it to work on a-one seal

--- tplt-search/search-template.py
+++ tplt-search/search-template.py
@@ -11,12 +11,13 @@ template = cv2.imread(sys.argv[2], 0)  # Load template as grayscale
 
 # Get the width and height of the template
 w, h = template.shape[::-1]
+print("Size of template:",w,h)
 
 # Perform template matching
 res = cv2.matchTemplate(main_gray, template, cv2.TM_CCOEFF_NORMED)
 
 # Define a threshold to find matches
-threshold = 0.8
+threshold = 0.7
 loc = np.where(res >= threshold)
 
 # Draw rectangles around the matched regions
@@ -25,6 +26,8 @@ for pt in zip(*loc[::-1]):
 
 # Display the result
 #cv2.imshow('Result', main_image)
-cv2.imwrite("balaji-marked.jpg", main_image)
+main_file = sys.argv[1]
+main_path = main_file.split(".")
+cv2.imwrite(main_path[0]+"-marked."+main_path[1], main_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
